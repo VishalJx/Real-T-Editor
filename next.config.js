87823c76de3dde897fb913b40b17bde2c1cd1945
Vishal Jaiswal <itsmevishal360@gmail.com>
2023-12-22 05:59:56 +0530

    disbaled babel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    babel: {
+        disable: true,
+    },
+}
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,4 +34,5 @@ module.exports = {
   plugins: [
     require('tailwind-scrollbar'),
   ],
+  
 }
